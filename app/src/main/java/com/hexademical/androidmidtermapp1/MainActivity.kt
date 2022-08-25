1c636069e5bd6686244e6f9e22f8740a80eee002
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2022-08-25 16:11:29 +0700

    add-log: on click hello button show full name and student id

--- app/src/main/java/com/hexademical/androidmidtermapp1/MainActivity.kt
+++ app/src/main/java/com/hexademical/androidmidtermapp1/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.hexademical.androidmidtermapp1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val helloButton: Button = findViewById(R.id.hello_button)
+        helloButton.setOnClickListener {
+            val fullName:TextView = findViewById(R.id.full_name)
+            val studentID:TextView = findViewById(R.id.student_id)
+            Log.d(TAG, "Full Name: ${fullName.text} | Stuident ID: ${studentID.text}" )
+        }
     }
 }
