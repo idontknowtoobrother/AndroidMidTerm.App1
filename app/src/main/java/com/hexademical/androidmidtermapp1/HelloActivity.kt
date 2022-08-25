8d377887c35dc117f15eb2118e379e2ef798c50d
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2022-08-25 16:22:58 +0700

    added: HelloActivity.kt, design: Hello set id as "hello_id" and Display full name id as "display_full_name"

---
+++ app/src/main/java/com/hexademical/androidmidtermapp1/HelloActivity.kt
@@ -0,0 +1,11 @@
+package com.hexademical.androidmidtermapp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HelloActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello)
+    }
+}
